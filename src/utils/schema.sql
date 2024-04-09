084a1cf54c3544317376984dd09973a5e76c4532
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2024-04-09 03:06:50 -0300

    refac: schema to use uuid in our id

--- src/utils/schema.sql
+++ src/utils/schema.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE IF NOT EXISTS projeto_vaga_backend;
 
 CREATE TABLE IF NOT EXISTS `projeto_vaga_backend`.`tb_departament` (
-  `uuid` VARCHAR(100) NOT NULL AUTO_INCREMENT,
+  `id` VARCHAR(100) NOT NULL,
   `name` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`uuid`)
+  PRIMARY KEY (`id`)
   );
 
-CREATE TABLE IF NOT EXISTS `projeto_vaga_backend`.`tb_employee` (
-    `uuid` VARCHAR(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `projeto_vaga_backend`.`tb_employeer` (
+    `id` VARCHAR(100) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
-    `departament_uuid` VARCHAR(100) NOT NULL,
+    `departament_id` VARCHAR(100) NOT NULL,
     `dependents` INT NOT NULL,
-    PRIMARY KEY (`uuid`),
-    FOREIGN KEY (`departament_uuid`) REFERENCES `tb_departament`(`uuid`)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`departament_id`) REFERENCES `tb_departament`(`id`)
     );
+    
