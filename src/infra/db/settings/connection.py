4da3f708e424f3068e4bb9d7b982615226efd114
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2024-04-09 03:08:06 -0300

    feat: connection with db and test connection

--- src/infra/db/settings/connection.py
+++ src/infra/db/settings/connection.py
@@ -7,11 +7,19 @@ class Connection:
     __connection_string: str
     __engine: create_engine
 
+    """
+    Initializes the connection string and the engine
+    """
     def __init__(self) -> None:
         self.__connection_string = "mysql+pymysql://loren:loren@localhost:3306/projeto_vaga_backend"
         self.__engine= self.__connect()
-    def __connect(self):
+    """
+    Create a connection with the database
+    """
+    def __connect(self) -> create_engine:
         return create_engine(self.__connection_string)
-
-    def get_connection(self):
+    """
+    To get the connection with the database
+    """
+    def get_connection(self) -> create_engine:
         return self.__engine
--- src/infra/db/settings/connection_test.py
+++ src/infra/db/settings/connection_test.py
@@ -1,5 +1,15 @@
-from connection import Connection
+import pytest
+from .connection import  Connection
 
+
+"""
+This test is skipped because it is necessary to have a database to run it.
+"""
+@pytest.mark.skip(reason="Sensitive test, it's necessary a database to run this test")
 def test_connection():
-    connection = Connection()
-    assert connection.get_connection() is not None
+      """
+      Test if the connection is not None,
+      if the connection is None, the test will fail.
+      """
+      connection = Connection()
+      assert connection.get_connection() is not None
